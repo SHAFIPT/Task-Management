3bb39811f232eb5c6f06d6418303da23f096d75e
SHAFIPT <shafipt077@gamil.com>
2025-03-22 19:58:20 +0530

    new updated

--- backend/src/controller/auth.ts
+++ backend/src/controller/auth.ts
@@ -17,7 +17,7 @@ export class AuthController implements IAuthController {
     private readonly REFRESH_TOKEN_COOKIE_OPTIONS: CookieOptions = {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        // sameSite: 'strict',
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     };
 
