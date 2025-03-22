94e2e0d1f6dcdc1ff86c384daa9f1d2c90590312
SHAFIPT <shafipt077@gamil.com>
2025-03-22 22:12:21 +0530

    feat : update Http only cookies

--- backend/src/controller/auth.ts
+++ backend/src/controller/auth.ts
@@ -16,8 +16,8 @@ export class AuthController implements IAuthController {
 
     private readonly REFRESH_TOKEN_COOKIE_OPTIONS: CookieOptions = {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        // sameSite: 'strict',
+        secure: true, // Always use secure in production
+        sameSite: 'none', // Critical for cross-domain requests
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     };
 
